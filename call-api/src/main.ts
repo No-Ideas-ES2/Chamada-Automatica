f266b32cdbaeb1b2ad14c012f19dd0994eda1368
Tiago Debastiani <debastianister@gmail.com>
2020-10-22 08:51:19 -0300

    Configurações pro Heroku

--- call-api/src/main.ts
+++ call-api/src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { AllExceptionsFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
-  app.useGlobalFilters(new AllExceptionsFilter());
-  
-  await app.listen(3000);
+  app.useGlobalFilters(new AllExceptionsFilter())
+  app.enableCors()
+  await app.listen(process.env.PORT || 3000)
 }
-bootstrap();
+bootstrap()
