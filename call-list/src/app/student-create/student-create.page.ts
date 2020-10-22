acca9eaef9d705f98ebe8b2569c11a053a7dabe3
jardeleko <jardelduarte594@gmail.com>
2020-10-22 15:26:44 -0300

    return all

---
+++ call-list/src/app/student-create/student-create.page.ts
@@ -0,0 +1,33 @@
+//student-create.page.ts
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/student';
+import { ApiService } from '../services/api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-student-create',
+  templateUrl: './student-create.page.html',
+  styleUrls: ['./student-create.page.scss'],
+})
+export class StudentCreatePage implements OnInit {
+
+  data: Student
+
+  constructor(
+    public apiService: ApiService,
+    public router: Router
+  ) {
+    this.data = new Student();
+  }
+
+  ngOnInit() {
+  }
+
+  submitForm() {
+    this.apiService.createItem(this.data).subscribe((response) => {
+      this.router.navigate(['student-list']);
+    });
+
+  }
+
+}
